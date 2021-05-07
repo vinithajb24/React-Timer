45be7b340b3a2581c416ab883d67b4ab1cc81d02
Vintha B <vintha@Shanthamurthys-MacBook-Pro.local>
2021-05-07 12:55:36 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Timer from './Timer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Welcome to Timer App built in React js</h2>
+      </div>
+        <Timer start={Date.now()} />
     </div>
   );
 }
